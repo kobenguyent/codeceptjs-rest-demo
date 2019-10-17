c56b841ea98ecc49c9cbb64693f47fe01d7e660c
PeterNgTr <peter.nguyentr@gmail.com>
2019-10-17 11:36:54 +0200

    fix point to wrong folder

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 npx codeceptjs run --steps
-npx allure generate allure-report -o allure-results
-ls allure-results
+# npx allure generate allure-report -o allure-results
+# ls allure-results
