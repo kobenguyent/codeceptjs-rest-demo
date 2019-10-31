b24cd71ae5ecbd9687d4c379d34e82c20b9c821a
PeterNgTr <peter.nguyentr@gmail.com>
2019-10-31 10:05:02 +0100

    add the uploading a file test

--- test/POST_test.js
+++ test/POST_test.js
@@ -1,7 +1,11 @@
 const expect = require('chai').expect;
 const faker = require('faker');
 const {I} = inject();
+const FormData = require('form-data');
+const fs = require('fs');
 let userData;
+let form = new FormData();
+
 
 Feature('POST tests');
 
@@ -14,4 +18,11 @@ Scenario('Verify creating new user', async () => {
 	const res = await I.sendPostRequest('/api/users', userData);
 	expect(res.status).to.eql(201);
 	expect(res.data.name).to.eql(userData.name);
+});
+
+Scenario('Verify uploading a file', async () => {
+	form.append('attachment', fs.createReadStream('test/fixtures/test_image.png'));
+
+	const res = await I.sendPostRequest('https://httpbin.org/post', form);
+	expect(res.status).to.eql(200);
 });
