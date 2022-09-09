e188a3303b4b74441eb43329b2d9d32b2ac333be
PeterNgTr <peter.nguyentr@gmail.com>
2022-09-09 11:14:39 +0200

    run tests with GA

--- codecept.conf.ts
+++ codecept.conf.ts
@@ -1,11 +1,3 @@
-import { setHeadlessWhen, setCommonPlugins } from '@codeceptjs/configure';
-// turn on headless mode when running with HEADLESS=true environment variable
-// export HEADLESS=true && npx codeceptjs run
-setHeadlessWhen(process.env.HEADLESS);
-
-// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
-setCommonPlugins();
-
 export const config: CodeceptJS.MainConfig = {
   tests: './*/*_test.ts',
   output: './output',
