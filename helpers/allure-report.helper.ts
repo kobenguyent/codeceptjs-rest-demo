e0ae561e869db867a2934f2d76269bf60e4b2249
kobenguyent <kobenguyent@gmail.com>
2025-05-19 16:30:10 +0200

    fix: allure env info

--- helpers/allure-report.helper.ts
+++ helpers/allure-report.helper.ts
@@ -61,7 +61,7 @@ class AllureHelper extends Helper {
       name: `Local Machine`,
       type: "local",
       reportName: `Test report on ${Date.now().toString()}`,
-      buildName: process.env.CI_BUILD_NUMBER || `Local-${Date.now()}`,
+      buildName: `Build #: ${process.env.CI_BUILD_NUMBER}` || `Local-${Date.now()}`,
     };
 
     writeFileSync(
